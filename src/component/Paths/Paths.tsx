b6e47b34676eff904ce2042751bdcc2af0de505b
shlande <shlandeme@gmail.com>
2023-04-10 16:28:13 +0800

    修复根目录时多一个路径按钮的问题

--- src/component/Paths/Paths.tsx
+++ src/component/Paths/Paths.tsx
@@ -11,11 +11,13 @@ export default (props: Pros) => {
   const paths = path.split("/")
   const elems = []
   elems.push(<Buttom key={"-1"} text={"/"} onClick={() => onPathChange("")}/>)
-  for (let i = 0; i < paths.length; i++) {
-    const path = paths.slice(0, i + 1).join("/")
-    elems.push(<Buttom key={i * 2} text={paths[i]} onClick={() => onPathChange(path)}></Buttom>)
-    if (i + 1 != paths.length) {
-      elems.push(<span key={i * 2 + 1}>/</span>)
+  if (path !=  "") {
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths.slice(0, i + 1).join("/")
+      elems.push(<Buttom key={i * 2} text={paths[i]} onClick={() => onPathChange(path)}></Buttom>)
+      if (i + 1 != paths.length) {
+        elems.push(<span key={i * 2 + 1}>/</span>)
+      }
     }
   }
   return <div className="paths">{elems}</div>
