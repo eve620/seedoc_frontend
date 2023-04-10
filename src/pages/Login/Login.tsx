f3ad2b8dbc48f6ce3f95a8e429b4e1252085f3d2
shlande <shlandeme@gmail.com>
2023-04-11 00:03:26 +0800

    stash

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,21 +1,21 @@
 import "./style.scss"
-import Buttom from "../../component/Buttom/Buttom";
+import Button from "../../component/Buttom/Buttom";
 
 export default () => {
   return (
     <div className="login">
       <div className="display">
-        <img src="/login-display.png" alt="login-display"/>
+        {/*<img src="/login-display.png" alt="login-display"/>*/}
       </div>
-      <div className="login-form">
-        <form>
-          <span>用户名</span>
-          <input type="text"/>
-          <span>密码</span>
-          <input type="text"/>
-        </form>
-        <div>
-          <Buttom text="登录"/>
+      <div className="login-form-container">
+        <div className="login-form">
+          <form>
+            <input type="text" placeholder="用户名"/>
+            <input type="text" placeholder="密码"/>
+          </form>
+          <div className="login-button">
+            <span>登录</span>
+          </div>
         </div>
       </div>
     </div>
