0ae762629f4f86a19cb71a658d8a76e0e909d6fc
shlande <shlandeme@gmail.com>
2023-04-11 01:15:16 +0800

    删除无用按钮

--- src/pages/TopBar/Menus.tsx
+++ src/pages/TopBar/Menus.tsx
@@ -3,7 +3,6 @@ import Avatar from "../../component/Avatar/Avatar";
 
 export default () => {
   return <div id="top-bar-menus">
-    <Buttom text="登录"/>
     <Buttom text="退出"/>
     <Avatar name="肖鹏飞"></Avatar>
   </div>
