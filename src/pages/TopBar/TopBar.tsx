731d1e49ba73065a480e539dc328fc6a54ade5e8
shlande <shlandeme@gmail.com>
2023-04-14 16:05:50 +0800

    logo点击可以回到首页

--- src/pages/TopBar/TopBar.tsx
+++ src/pages/TopBar/TopBar.tsx
@@ -1,15 +1,17 @@
 import "./style.scss"
 import "../../component/Button/Button"
 import "./Menus"
-import Buttom from "../../component/Button/Button";
 import Menus from "./Menus";
+import {useNavigate} from "react-router-dom";
+import {toMain} from "../../router";
 
 export default () => {
+  const navigate = useNavigate();
 
   return (
     <div id="top-bar">
       <div className="app-container" id="top-bar-content">
-        <div id="logo">
+        <div id="logo" onClick={() => toMain(navigate)}>
           <img src="/logo-on.png" height="30px" className="logo"></img>
           <span className="font-color-shadow"> | </span>
           <span className="font-content">本科教学文件管理系统</span>
