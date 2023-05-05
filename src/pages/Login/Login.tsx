74a0e3253660e4b4882b324385316852282b1120
shlande <shlandeme@gmail.com>
2023-05-06 04:07:02 +0800

    调整登录页面

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,11 @@
 import "./style.scss"
-import Input from "../../component/Input/Input";
 import {toMain} from "../../router";
-import {Handler as InputHandler} from "../../component/Input/Input"
+import Input, {Handler as InputHandler} from "../../component/Input/Input"
 import {NavigateFunction, useNavigate} from "react-router-dom";
 import {getInstance} from "../../sdk/Instance";
 import {useEffect, useRef} from "react";
 import Pop from "../../component/Pop/Pop";
+import Button from "../../component/Button/Button";
 
 export default () => {
   const navigate = useNavigate()
@@ -18,7 +18,7 @@ export default () => {
         toMain(navigate)
       }
     })
-  },[])
+  }, [])
   const onLogin = () => {
     const uname = username.current ? username.current.value() : undefined
     const pwd = password.current ? password.current.value() : undefined
@@ -36,28 +36,36 @@ export default () => {
     return getInstance().loginCas()
   }
   return (
-    <div className="login">
-      <div className="display">
-        <div>
-          <h1>本科教学文件管理系统</h1>
-        </div>
+    <div className={"container"}>
+      <div className={"login-header"}>
+        <img className={"login-logo"} src={"/logo-login.png"} alt={"校徽"}/>
+        <span>本科教学文件管理系统</span>
       </div>
-      <div className="login-form-container">
+      <div className="login-container">
+
+        <div className="login-display">
+          <img src="/login-display.webp" alt="校园图片" className="login-display-img"/>
+        </div>
         <div className="login-form">
-          <div className="title">登录</div>
-          <form>
-            <Input ref={username} size={"large"} placeHolder={"用户名"} type={"text"}/>
-            <Input ref={password} size={"large"} placeHolder={"密码"} type={"password"}/>
-          </form>
-          <div className="login-button-container">
-            <div onClick={onLogin} className="login-button">
-              <span>登录</span>
+          <div>
+            <h2>用户登录</h2>
+            <div className={"form-input"}>
+              <Input ref={username} size={"large"} placeHolder={"用户名"} type={"text"}></Input>
+              <Input ref={password} size={"large"} placeHolder={"密码"} type={"password"}></Input>
             </div>
-            <div onClick={onLoginCas} className="login-button">
-              <span>统一认证登录</span>
+            <div onClick={onLogin} className={"confirm"}><span>登录</span></div>
+
+            <div className={"other"}>
+              <div className={"spacer"}></div>
+              <div className={"other-text"}><span>或者使用</span></div>
+              <div className={"spacer"}/>
             </div>
+            <div onClick={onLoginCas} className={"login-option"}><img src={"/favicon.ico"} alt={"cas-logo"}/>统一认证登录</div>
           </div>
         </div>
+      </div>
+      <div className="copyright">
+        <p>开发维护：西安电子科技大学电子工程学院</p>
       </div>
     </div>
   )
